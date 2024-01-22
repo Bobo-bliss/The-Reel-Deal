2615d11b4cf254e979b7f8c62f82bc44bb656ca4
Rob Schultz <Robschultz4444@yahoo.com>
2024-01-22 12:44:26 -0600

    Changed header nav buttons no bg

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const mainSlideshow = document.querySelectorAll(".slideshow-main img");
 const header = document.querySelector("header");
 const headerTitle = document.querySelector("header a");
+const headerNav = document.querySelectorAll(".header-buttons button");
+console.log(headerNav);
 
 let currentSlide = 0;
 
@@ -21,10 +23,14 @@ window.addEventListener("scroll", () => {
   if (window.scrollY >= 10) {
     header.classList.add("scroll-fade-in");
     headerTitle.classList.add("header-title-scroll");
+    headerNav.forEach((nav) => nav.classList.add("home-header-nav-scroll"));
     header.classList.remove("scroll-fade-out");
+    headerNav.forEach((nav) => nav.classList.remove("home-header-nav"));
   } else {
     header.classList.remove("scroll-fade-in");
     headerTitle.classList.remove("header-title-scroll");
+    headerNav.forEach((nav) => nav.classList.remove("home-header-nav-scroll"));
     header.classList.add("scroll-fade-out");
+    headerNav.forEach((nav) => nav.classList.add("home-header-nav"));
   }
 });
